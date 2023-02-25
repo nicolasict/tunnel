ddc249bd69949f34ccd0dcd15322e7c380ae7201
nicolasict <niko.heri.setiyawan@gmail.com>
2023-02-26 05:35:27 +0700

    first commit

---
+++ menu/about.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+
+clear
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \E[40;1;37m        • PATUNGAN - GCP •            \e[0m"
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+
+
+
